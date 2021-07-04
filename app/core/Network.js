4a138341899b7aed7eedb0810b78414a00903357
russian-proger <sagitov.2806@mail.ru>
2021-07-04 16:20:08 +0500

    Server API was added

--- app/core/Network.js
+++ app/core/Network.js
@@ -4,8 +4,8 @@ export default function Network(self) {
   this.online = () => this.requestAPI({ action: 'online' });
 
   this.requestAPI = (params) => {
-    const seed = Math.random().toString(36).slice(2);
-    const secret = sha256.hmac(API_KEY, seed);
+    // const seed = Math.random().toString(36).slice(2);
+    // const secret = sha256.hmac(API_KEY, seed);
     return fetch(URI_PREFIX + "/api", {
       method: 'POST',
       // body: JSON.stringify({ ...params, uid: UID, secret, seed }),
@@ -15,4 +15,6 @@ export default function Network(self) {
       }
     }).then(res => res.json());
   }
+
+  this.
 }
--- server.js
+++ server.js
@@ -53,7 +53,111 @@ app.get("/", (req, res) => {
 })
 
 
+/**
+ * Обработка API
+ */
+ app.post('/api', async (req, res) => {
+  var response = { status: true };
+  const sendError = (message) => {
+    response.status = false;
+    response.message = message;
+    res.send(JSON.stringify(response));
+    res.end();
+    return ;
+  }
 
+  switch (req.body.action) {
+    case '': {
+
+      break;
+    }
+    case 'update-results': {
+      const s_fname = `./cache/skills/${req.body.uid}/${req.body.subject_id}`;
+      let skills = fs.readFileSync(s_fname).toString('utf8').split(' ').map(v => parseFloat(v));
+      req.body.results.forEach((v, i) => {
+        let d = 0.25 + 0.75 * (1 - Math.min(v.duration / 60000, 1));
+        skills[v.id] = skills[v.id] * 0.8 + (v.result * 20) * d;
+      });
+      fs.writeFileSync(s_fname, skills.join(' '));
+      
+      const f_fname = `./cache/freqs/${req.body.uid}/${req.body.subject_id}`;
+      let freqs = fs.readFileSync(f_fname).toString('utf8').split(' ').map(v => parseFloat(v));
+      req.body.results.forEach(v => ++freqs[v.id]);
+      fs.writeFileSync(f_fname, freqs.join(' '));
+      
+
+      let tasks_c = req.body.results.length;
+      let correct_c = 0;
+      let uid = req.body.uid;
+      let subject_id = req.body.subject_id;
+
+      req.body.results.forEach(v => correct_c += v.result);
+      const [{ insertId }] = await pool.execute(`INSERT INTO results(uid, tasks_c, correct_answers_c, subject_id) VALUES (${uid}, ${tasks_c}, ${correct_c}, ${subject_id});`);
+
+      let data = [tasks_c];
+      req.body.results.forEach(v => data.push(v.id, '"' + (v.answer == null || v.answer == undefined ? "" : v.answer) + '"', v.duration, v.result + 0));
+      fs.writeFileSync(`./cache/tests/${insertId}`, data.join(' '));
+      break;
+    }
+
+    case 'is-paid': {
+      let [[info]] = await pool.execute(`SELECT \`balance\` FROM \`users\` WHERE \`uid\`=${ req.body.uid }`);
+      response.status = true;
+      response.result = info.balance <= 1000;
+      break;
+    }
+
+    case 'get-skills': {
+      let fname = `./cache/skills/${req.body.uid}`;
+
+      if (!fs.existsSync(fname)) {
+        fs.mkdirSync(fname);
+        subjects.forEach((v, i) => fs.writeFileSync(`${fname}/${i}`, new Array(300).fill(50).join(" ")));
+      }
+
+      response.result = fs.readFileSync(`${fname}/${req.body.subject_id}`).toString('utf8').split(' ').map(v => parseInt(v));
+      break;
+    }
+
+    case 'get-freqs': {
+      let fname = `./cache/freqs/${req.body.uid}`;
+
+      if (!fs.existsSync(fname)) {
+        fs.mkdirSync(fname);
+        subjects.forEach((v, i) => fs.writeFileSync(`${fname}/${i}`, new Array(300).fill(0).join(" ")));
+      }
+
+      response.result = fs.readFileSync(`${fname}/${req.body.subject_id}`).toString('utf8').split(' ').map(v => parseInt(v));
+      break;
+    }
+
+    case 'get-stats': {
+      if (!req.body.hasOwnProperty('subject_id')) {
+        response.status = false;
+        break;
+      }
+
+      let [results] = await pool.execute(`SELECT * FROM results WHERE uid=${req.body.uid} AND subject_id=${req.body.subject_id} ORDER BY id DESC`);
+      response.counts = (await pool.execute(`
+        SELECT
+          SUM(tasks_c) AS tasks_c,
+          SUM(correct_answers_c) AS correct_answers_c,
+          AVG(tasks_c) AS avg_tasks_c,
+          AVG(correct_answers_c) AS avg_correct_answers_c
+        FROM results
+        WHERE uid=${req.body.uid} AND subject_id=${req.body.subject_id}`)
+      )[0][0];
+      response.results = results;
+      break;
+    }
+
+    default: {
+      return sendError("Параметр `action` некорректный");
+    }
+  }
+
+  res.send(JSON.stringify(response));
+});
 
 // Запуск сервера
 const PORT = 10000;

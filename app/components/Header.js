4bda860de300e8e4175650cf3d397ac287c64672
russian-proger <sagitov.2806@mail.ru>
2021-07-04 14:11:40 +0500

    Header edited

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,13 +5,13 @@ import "./Header.sass";
 export default function Header() {
   return (
     <header className="app-header">
-      <div className="logo"><img src="/static/images/7.spirit.13.png" /></div>
-      <div className="team-name">
-        <span>7.spirit.13</span>
-      </div>
       <div className="app-title">
         <span>Мониторинг строительных ресурсов</span>
       </div>
+      <div className="team-name">
+        <span>Team:  7.spirit.13</span>
+      </div>
+      <div className="logo"><img src="/static/images/7.spirit.13.png" /></div>
     </header>
   );
 }
